03c8ab793e51858e2382dc3832e55e63f3be7d25
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-11-25 09:19:40 +0100

    Start

---
+++ src/main/java/org/example/esportkalendereks/ESportKalenderEksApplication.java
@@ -0,0 +1,13 @@
+package org.example.esportkalendereks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportKalenderEksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESportKalenderEksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/esportkalendereks/ESportKalenderEksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.esportkalendereks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESportKalenderEksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
