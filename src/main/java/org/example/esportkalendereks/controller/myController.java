182c002d9bb4d0a6cd38db6388f7c66f1faec728
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-11-26 15:06:47 +0100

    Fixing running app

--- src/main/java/org/example/esportkalendereks/controller/myController.java
+++ src/main/java/org/example/esportkalendereks/controller/myController.java
@@ -1,11 +1,13 @@
 package org.example.esportkalendereks.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class myController {
 
-    @GetMapping("/") //VIRKER IKKE IDK WHY
+    @GetMapping("/") // This will now resolve to the forside.html template
     public String forside() {
-        return "forside.html";
+        return "forside";
     }
 }
