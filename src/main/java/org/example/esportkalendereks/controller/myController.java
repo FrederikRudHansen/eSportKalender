74b15758ec98afc68fe56de0a3b69ca8a4bc6501
AssilAttiah <asat0001@stud.kea.dk>
2024-11-26 14:45:12 +0100

    Forside added logo og header er done. MAngler links til andre sider i header + i "spil" listen.

--- src/main/java/org/example/esportkalendereks/controller/myController.java
+++ src/main/java/org/example/esportkalendereks/controller/myController.java
@@ -1,4 +1,11 @@
 package org.example.esportkalendereks.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class myController {
+
+    @GetMapping("/") //VIRKER IKKE IDK WHY
+    public String forside() {
+        return "forside.html";
+    }
 }
