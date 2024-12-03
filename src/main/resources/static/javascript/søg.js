31b98f368ac50432969b8febe97e9088aab2403e
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-12-03 11:00:26 +0100

    Prototype af ny stil søg done, mangler kun funktionen

---
+++ src/main/resources/static/javascript/søg.js
@@ -0,0 +1,6 @@
+console.log("Søgesystem klar!");
+
+// Placeholder for søgefunktion
+document.getElementById('searchBox').addEventListener('input', function () {
+    console.log("Søger efter:", this.value);
+});
