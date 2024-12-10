ecfbd79b27d75320bea2630c7d4522da074afc05
AssilAttiah <asat0001@stud.kea.dk>
2024-12-10 14:32:54 +0100

    GET Mappings er korrekte til tnt sidene

--- src/main/java/org/example/esportkalendereks/controller/MapController.java
+++ src/main/java/org/example/esportkalendereks/controller/MapController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MapController {
 
+    @GetMapping("tipsntricks")
+    public String tipsntricks() {
+        return "tnt/tipsntricks";
+    }
+
     @GetMapping("dust2")
     public String dust2() {
-        return "tipsntrickshtmler/dust2";
+        return "tnt/dust2";
+    }
+
+    @GetMapping("baldurstips")
+    public String baldurstips() {
+        return "tnt/baldurstips";
+    }
+
+    @GetMapping("cstips")
+    public String cstips() {
+        return "tnt/cstips";
     }
 }
